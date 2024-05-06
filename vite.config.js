9c2e174fa5b05f13e6bf92c0e97aa88481874b8e
Alberto Ruis <alber.ruis2886@gmail.com>
2024-05-06 15:04:11 -0300

    config vite base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://AlberRuis86.github.io/Macetas-DyS"
+  base: "/Macetas-DyS"
 })
